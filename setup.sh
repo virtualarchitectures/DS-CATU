745a6649986eb1d7100814df5b85c60f28df956e
virtualarchitectures <virtualarchitectures@users.noreply.github.com>
2024-03-20 20:05:21 +0000

    Added code to download miniconda if not installed

--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Initialize Conda if not already initialized
+# Initialise existing Conda installation if not already initialised
 eval "$(conda shell.bash hook)"
 
 # Define the name of your Conda environment
 ENV_NAME="DS-CATU"
+
+# Check if Miniconda is already installed
+if command -v conda &> /dev/null; then
+    echo "Miniconda is already installed"
+else
+    echo "Installing Miniconda..."
+    # Make directory for Miniconda installation
+    mkdir -p ~/miniconda3 || error_exit "Failed to create Miniconda directory"
+    # Download Miniconda installer
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh || error_exit "Failed to download Miniconda installer"
+    # Run Miniconda installer
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3 || error_exit "Failed to install Miniconda"
+    # Remove Miniconda installer
+    rm -rf ~/miniconda3/miniconda.sh || error_exit "Failed to remove Miniconda installer"
+    echo "Miniconda installed successfully and initialised"
+    # Initialise miniconda
+    ~/miniconda3/bin/conda init bash
+fi
 
 # Create new Conda environment
 echo "Creating Conda environment..."
